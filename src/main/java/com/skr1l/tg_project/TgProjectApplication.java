99bd5e715b3797157d66cbd421d191a0309c0f39
Skrill <ivan.samal@student.ehu.lt>
2026-03-20 21:40:03 +0300

    Initial commit

---
+++ src/main/java/com/skr1l/tg_project/TgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skr1l.tg_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skr1l/tg_project/TgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr1l.tg_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
